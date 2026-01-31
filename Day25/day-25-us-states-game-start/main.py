da922732e41ebf8a0a3a63b4a3eff812242b7e93
Pedro Lucas <pedrolucas.js18@gmail.com>
2026-01-31 00:06:51 -0300

    :sparkles: Feat: Add projeto Day30

--- Day25/day-25-us-states-game-start/main.py
+++ Day25/day-25-us-states-game-start/main.py
@@ -13,13 +13,11 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in states_list:
-            if state not in guessed_states:
-                missing_states.append(state)
-            new_data = pandas.DataFrame(missing_states)
-            new_data.to_csv("states_to_learn.csv")
+        missing_states = [state for state in states_list if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
+    
     if answer_state in states_list and answer_state not in guessed_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
--- Day29/password-manager-start/main.py
+++ Day29/password-manager-start/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import random, pyperclip
+import random, pyperclip, json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -13,14 +13,14 @@ def generate_password():
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
     
-    password_list = [random.choice(pool) for pool, count in [(letters, nr_letters), (symbols, nr_symbols), (numbers, nr_numbers)] 
-                    for _ in range(count)]
-
     """
     password_list = [random.choice(letters) for letter in range(nr_letters)]
     password_list = [random.choice(symbols) for symbol in range(nr_symbols)]
     password_list = [random.choice(numbers) for number in range(nr_numbers)] 
     """
+    password_list = [random.choice(pool) for pool, count in [(letters, nr_letters), (symbols, nr_symbols), (numbers, nr_numbers)] 
+                    for _ in range(count)]
+    
     random.shuffle(password_list)
     password = "".join(password_list)
     password_entry.insert(0, password)
@@ -31,17 +31,48 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email" : email,
+            "password" : password,
+        }
+    }  
     
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo("Oops", "Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(website, f"These are the details entered \nEmail: {email}\nPassword: {password}\nIt's ok to save?")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
-                
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file) 
+        except (FileNotFoundError, json.decoder.JSONDecodeError):        
+            # Updating old data with new data
+            data = new_data
+        else:
+            data.update(new_data)  
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4) # Argumentos: dados a serem passados, arquivo destino, identação                          
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+            
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+                    
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -71,6 +102,8 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2, sticky="EW")
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2, sticky="EW")
 add_button = Button(text="Add", width=36, command=save)
